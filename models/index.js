b2f13a1a93724823a24672508aa513b0f44e108b
Stephanie Lee <stephaniemjlee@gmail.com>
2021-06-03 10:21:55 +1000

    updated models

--- models/index.js
+++ models/index.js
@@ -11,10 +11,28 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+})
 
 module.exports = {
   Product,
